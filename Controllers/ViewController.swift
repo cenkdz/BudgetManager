6bc8e926b82f30d7a05dbe78f2b76b6d39926cb5
Cenk Donmez <56077278+cenkdz@users.noreply.github.com>
2020-12-01 16:49:56 +0300

    Initial commit

---
+++ Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BudgetManager
+//
+//  Created by Cenk Donmez on 1.12.2020.
+//  Copyright © 2020 Cenk Donmez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
